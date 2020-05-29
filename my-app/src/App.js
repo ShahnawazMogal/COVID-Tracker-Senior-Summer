9ba841e9d3ce529c8d4838f4003082171428049c
Shahnawaz Mogal <shahnawazbaigmogal@outlook.com>
2020-05-30 00:22:05 +0300

    Added CountUp, ',' for numbers, animated loading signs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,9 @@ import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Badge from "react-bootstrap/Badge";
+import Spinner from "react-bootstrap/Spinner";
 import { Bar, Line } from "react-chartjs-2";
+import CountUp from "react-countup";
 import axios from "axios";
 
 const url = "https://coronavirus-19-api.herokuapp.com/countries/bahrain"; //for card data
@@ -65,177 +67,222 @@ class App extends React.Component {
             <Navbar.Text>Developed by Shahnawaz Mogal </Navbar.Text>
           </Navbar.Collapse>{" "}
         </Navbar>
-        <Jumbotron>
-          <h2>Current Case Summary</h2>
 
-          <CardDeck>
-            <Card border="warning" bg="warning" style={{ width: "12rem" }}>
-              <Card.Header>Active Cases</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  {bhdata.active + " "}
-                  <Badge pill variant="secondary">
-                    Today: {bhdata.todayCases}
-                  </Badge>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-            <Card border="danger" bg="danger" style={{ width: "12rem" }}>
-              <Card.Header>Critical</Card.Header>
-              <Card.Body>
-                <Card.Title>{bhdata.critical}</Card.Title>
-              </Card.Body>
-            </Card>
-          </CardDeck>
-        </Jumbotron>
+        {!bhdata.active && ( //In case API call is taking time
+          <div>
+            <Spinner animation="grow" variant="primary" />
+            <Spinner animation="grow" variant="secondary" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="info" />
+          </div>
+        )}
+        {bhdata.active && ( //Only render this part after API call success
+          <div className="App">
+            <Jumbotron>
+              <h2>Current Case Summary</h2>
 
-        <Jumbotron>
-          <h2>Historical Case Summary</h2>
-          <CardDeck>
-            <Card border="primary" bg="primary" style={{ width: "12rem" }}>
-              <Card.Header>Tests Performed</Card.Header>
-              <Card.Body>
-                <Card.Title>{bhdata.totalTests}</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card border="secondary" bg="secondary" style={{ width: "12rem" }}>
-              <Card.Header>Cases</Card.Header>
-              <Card.Body>
-                <Card.Title>{bhdata.cases}</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card border="success" bg="success" style={{ width: "12rem" }}>
-              <Card.Header>Recoveries</Card.Header>
-              <Card.Body>
-                <Card.Title>{bhdata.recovered}</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card border="danger" bg="danger" style={{ width: "12rem" }}>
-              <Card.Header>Deaths</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  {bhdata.deaths + " "}
-                  <Badge pill variant="secondary">
-                    Today: {bhdata.todayDeaths}
-                  </Badge>
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </CardDeck>
-        </Jumbotron>
+              <CardDeck>
+                <Card border="warning" bg="warning" style={{ width: "12rem" }}>
+                  <Card.Header>Active Cases</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.active}
+                        duration={2.5}
+                        separator=","
+                      />
+                      <Badge pill variant="secondary">
+                        Today:{" "}
+                        <CountUp
+                          start={0}
+                          end={bhdata.todayCases}
+                          duration={2.5}
+                          separator=","
+                        />
+                      </Badge>
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+                <Card border="danger" bg="danger" style={{ width: "12rem" }}>
+                  <Card.Header>Critical</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.critical}
+                        duration={2.5}
+                        separator=","
+                      />
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </CardDeck>
+            </Jumbotron>
 
-        <Jumbotron>
-          <h2>Charts</h2>
-          <Bar
-            data={{
-              labels: ["Infected", "Recovered", "Deaths"],
-              datasets: [
-                {
-                  label: "People",
-                  backgroundColor: [
-                    "rgba(255, 193, 7, 1)",
-                    "rgba(40, 167, 69, 1)",
-                    "rgba(220, 53, 69, 1)",
+            <Jumbotron>
+              <h2>Historical Case Summary</h2>
+              <CardDeck>
+                <Card border="primary" bg="primary" style={{ width: "12rem" }}>
+                  <Card.Header>Tests Performed</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.totalTests}
+                        duration={2.5}
+                        separator=","
+                      />
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+                <Card
+                  border="secondary"
+                  bg="secondary"
+                  style={{ width: "12rem" }}
+                >
+                  <Card.Header>Cases</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.cases}
+                        duration={2.5}
+                        separator=","
+                      />
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+                <Card border="success" bg="success" style={{ width: "12rem" }}>
+                  <Card.Header>Recoveries</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.recovered}
+                        duration={2.5}
+                        separator=","
+                      />
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+                <Card border="danger" bg="danger" style={{ width: "12rem" }}>
+                  <Card.Header>Deaths</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      <CountUp
+                        start={0}
+                        end={bhdata.deaths}
+                        duration={2.5}
+                        separator=","
+                      />
+                      <Badge pill variant="secondary">
+                        Today:{" "}
+                        <CountUp
+                          start={0}
+                          end={bhdata.todayDeaths}
+                          duration={2.5}
+                          separator=","
+                        />
+                      </Badge>
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </CardDeck>
+            </Jumbotron>
+
+            <Jumbotron>
+              <h2>Charts</h2>
+              <Bar
+                data={{
+                  labels: ["Infected", "Recovered", "Deaths"],
+                  datasets: [
+                    {
+                      label: "People",
+                      backgroundColor: [
+                        "rgba(255, 193, 7, 1)",
+                        "rgba(40, 167, 69, 1)",
+                        "rgba(220, 53, 69, 1)",
+                      ],
+                      data: [bhdata.cases, bhdata.recovered, bhdata.deaths],
+                    },
                   ],
-                  data: [bhdata.cases, bhdata.recovered, bhdata.deaths],
-                },
-              ],
-            }}
-            width={650}
-            height={300}
-            options={{
-              legend: { display: false },
-              maintainAspectRatio: false,
-            }}
-          />
-        </Jumbotron>
-        <Jumbotron>
-          <Line
-            data={{
-              labels: dailyData.map(({ date }) => date),
-              datasets: [
-                {
-                  data: dailyData.map((data) => data.confirmed),
-                  label: "Infected",
-                  borderColor: "#ffc107",
-                  backgroundColor: "rgba(255, 193, 7, 0.5)",
-                  fill: true,
-                },
-                {
-                  data: dailyData.map((data) => data.recovered),
-                  label: "Recovered",
-                  borderColor: "rgba(40, 167, 69, 1)",
-                  backgroundColor: "rgba(40, 167, 69, 0.5)",
-                  fill: true,
-                },
-                {
-                  data: dailyData.map((data) => data.deaths),
-                  label: "Deaths",
-                  borderColor: "rgba(220, 53, 69, 1)",
-                  backgroundColor: "rgba(220, 53, 69, 0.5)",
-                  fill: true,
-                },
-              ],
-            }}
-            width={650}
-            height={300}
-            options={{
-              legend: { display: true },
-              maintainAspectRatio: false,
-            }}
-          />
-        </Jumbotron>
+                }}
+                width={650}
+                height={300}
+                options={{
+                  legend: { display: false },
+                  maintainAspectRatio: false,
+                }}
+              />
+            </Jumbotron>
+            <Jumbotron>
+              <Line
+                data={{
+                  labels: dailyData.map(({ date }) => date),
+                  datasets: [
+                    {
+                      data: dailyData.map((data) => data.confirmed),
+                      label: "Infected",
+                      borderColor: "#ffc107",
+                      backgroundColor: "rgba(255, 193, 7, 0.5)",
+                      fill: true,
+                    },
+                    {
+                      data: dailyData.map((data) => data.recovered),
+                      label: "Recovered",
+                      borderColor: "rgba(40, 167, 69, 1)",
+                      backgroundColor: "rgba(40, 167, 69, 0.5)",
+                      fill: true,
+                    },
+                    {
+                      data: dailyData.map((data) => data.deaths),
+                      label: "Deaths",
+                      borderColor: "rgba(220, 53, 69, 1)",
+                      backgroundColor: "rgba(220, 53, 69, 0.5)",
+                      fill: true,
+                    },
+                  ],
+                }}
+                width={650}
+                height={300}
+                options={{
+                  legend: { display: true },
+                  maintainAspectRatio: false,
+                }}
+              />
+            </Jumbotron>
 
-        <Jumbotron>
-          <h2>Information from Ministry of Health</h2>
-          <CardDeck>
-            <Card>
-              <iframe
-                src="https://www.moh.gov.bh/COVID19/News"
-                width="100%"
-                height="500"
-              >
-                <p>Your browser does not support iframes.</p>
-              </iframe>
-            </Card>
-            <Card>
-              <a
-                class="twitter-timeline"
-                href="https://twitter.com/MOH_Bahrain"
-                data-chrome="nofooter"
-                height="500"
-                width="100%"
-              ></a>
-            </Card>
-          </CardDeck>
-        </Jumbotron>
+            <Jumbotron>
+              <h2>Information from Ministry of Health</h2>
+              <CardDeck>
+                <Card>
+                  <iframe
+                    src="https://www.moh.gov.bh/COVID19/News"
+                    width="100%"
+                    height="600"
+                  >
+                    <p>Your browser does not support iframes.</p>
+                  </iframe>
+                </Card>
+                <Card>
+                  <a
+                    class="twitter-timeline"
+                    href="https://twitter.com/MOH_Bahrain"
+                    data-chrome="nofooter"
+                    height="600"
+                    width="100%"
+                  ></a>
+                </Card>
+              </CardDeck>
+            </Jumbotron>
+          </div>
+        )}
       </div>
     );
   }
 }
 
 export default App;
-
-/*
-<Jumbotron>
-          <iframe
-            src="http://www.youtube.com/embed/?listType=user_uploads&list=TheMoHBahrain"
-            width="800"
-            height="400"
-          ></iframe>
-        </Jumbotron>
- */
-/*
-<Timeline
-        dataSource={{
-          sourceType: "profile",
-          screenName: "MOH_Bahrain",
-        }}
-        options={{
-          username: "MOH_Bahrain",
-          width: "450",
-          height: "650",
-        }}
-        onLoad={() => console.log("Timeline is loaded!")}
-      />
-*/
